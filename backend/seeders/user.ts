2652acbf082cdd6180b1becb95a091f88c77e4af
AndyWidianto <gamestory5001@gmail.com>
2026-06-28 22:09:36 +0700

    update seeder user

--- backend/seeders/user.ts
+++ backend/seeders/user.ts
@@ -1,5 +1,5 @@
-const { PrismaClient, UserRole } = require("@prisma/client");
-const bcrypt = require("bcryptjs");
+import { PrismaClient, UserRole } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
@@ -9,61 +9,19 @@ export async function userSeeders() {
 
   const users = [
     {
-      name: 'Andy Widianto',
-      email: 'andy@evolve.id',
-      password: "andy1234567&",
-      role: UserRole.admin,
-      username: "andy124",
-      updatedAt: new Date(), 
-    },
-    {
-      name: 'Budi Santoso',
-      email: 'budi@desa.go.id',
-      password: "budy12345678$",
-      role: UserRole.admin,
-      username: "budy1234",
-      updatedAt: new Date(Date.now() - 48 * 60 * 60 * 1000), // Inaktif (2 hari lalu)
-    },
-    {
-      name: 'Siti Aminah',
-      email: 'siti@desa.go.id',
-      password: "password1234",
-      role: UserRole.admin,
-      username: "siti123",
-      updatedAt: new Date(),
-    },
-    {
-      name: 'Admin Desa',
-      email: 'admin@desa.go.id',
-      password: "admin09876545&",
-      role: UserRole.admin,
-      username: "admin1234",
-      updatedAt: new Date(),
-    },
-    {
-      name: 'User Testing',
-      email: 'test@example.com',
-      password: "testing1239654&",
-      role: UserRole.admin,
-      username: "test1234",
-      updatedAt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
-    },
-    {
-      name: 'Review',
-      email: 'review@example.com',
-      password: "preview1234",
-      role: UserRole.review,
-      username: "reviewer",
-      updatedAt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
-    },
-    {
-      name: 'andy',
-      email: 'andywidianto56@gmail.com',
-      password: "developer1234&",
+      name: "Kepala Desa",
+      email: process.env.EMAIL_KEPALA_DESA!,
+      password: process.env.PASSWORD_KEPALA_DESA!,
       role: UserRole.super_admin,
-      username: "andywidianto",
-      updatedAt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
+      username: process.env.USERNAME_KEPALA_DESA!,
     },
+    {
+      name: "admin",
+      email: process.env.EMAIL_ADMIN!,
+      password: process.env.PASSWORD_ADMIN!,
+      role: UserRole.admin,
+      username: "admin",
+    }
   ];
 
   for (const user of users) {
@@ -80,3 +38,5 @@ export async function userSeeders() {
 
   console.log('✅ User seeding completed.');
 }
+
+userSeeders();
