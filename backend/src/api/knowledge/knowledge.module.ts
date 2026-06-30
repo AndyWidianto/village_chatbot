9d705d009ed5e1393af6354323c80881947769dc
AndyWidianto <gamestory5001@gmail.com>
2026-06-30 21:21:43 +0700

    fix: error create knowledge base

--- backend/src/api/knowledge/knowledge.module.ts
+++ backend/src/api/knowledge/knowledge.module.ts
@@ -3,10 +3,11 @@ import { KnowledgeService } from "./knowledge.service";
 import { OllamaService } from "../../lib/agent/ollama.service";
 import { KnowledgeController } from "./knowledge.controller";
 import { AccessTokenStrategy } from "@/lib/guards/access.strategy";
+import { GeminiService } from "@/lib/agent/gemini.service";
 
 
 @Module({
-    providers: [KnowledgeService, OllamaService, AccessTokenStrategy],
+    providers: [KnowledgeService, OllamaService, AccessTokenStrategy, GeminiService],
     controllers: [KnowledgeController]
 })
 export class KnowledgeModule {}
